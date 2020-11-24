fc0d9fd56044619dcd097ce75bf0b0896912f7f9
honestoughN <honest.tough18@gmail.com>
2020-11-24 20:23:51 +0900

    投稿機能実装

--- app/controllers/hearts_controller.rb
+++ app/controllers/hearts_controller.rb
@@ -2,7 +2,22 @@ class HeartsController < ApplicationController
   def index
     @hearts = Heart.all
   end
+
   def new
     @heart = Heart.new
+  end
+
+  def create
+    @heart = Heart.new(heart_params)
+    if @heart.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def heart_params
+    params.require(:heart).permit(:title, :detail, :category_id).merge(user_id: current_user.id)
   end
 end
