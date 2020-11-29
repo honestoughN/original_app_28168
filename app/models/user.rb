fa1ba505ea40be6c5ff16a7d1f3b79833fbe616c
honestoughN <honest.tough18@gmail.com>
2020-11-29 18:05:39 +0900

    ユーザ登録時のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
 
   has_many :hearts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true, format: { with: /@/, message: "は@を含めて入力して下さい" }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+{6,}\z/i, message: "は半角英数字6文字以上で入力して下さい"}
+  end
 end
