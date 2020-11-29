8bcb35f16adaab7da29f2f4014cfa330cc93c197
honestoughN <honest.tough18@gmail.com>
2020-11-29 17:55:37 +0900

    投稿機能、バリデーション設定

--- app/models/heart.rb
+++ app/models/heart.rb
@@ -1,4 +1,12 @@
 class Heart < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
   has_many :comments, dependent: :destroy
+
+  with_options presence: true do
+    validates :title
+    validates :detail
+    validates :category_id, numericality: { other_than: 1 }
+  end
 end
