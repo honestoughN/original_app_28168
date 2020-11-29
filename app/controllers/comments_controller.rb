887f7a269a3bb24a8ecb3946c6e367768b55347d
honestoughN <honest.tough18@gmail.com>
2020-11-29 17:06:26 +0900

    もっと聞きたいボタンの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
+  def new
+    @comments = Comment.all
+    @comment = Comment.new
+  end
+
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/hearts/#{comment.heart.id}"
+    @comment = Comment.new(comment_params)
+    heart = Heart.find(params[:heart_id])
+    user_name = current_user.nickname
+    if @comment.save
+      ActionCable.server.broadcast 'comment_channel', content: @comment, heart: heart, user_name: user_name
+    end
   end
 
   private
