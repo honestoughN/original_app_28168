92b8a2471c53c3f1ce73017bc575ce7ee137b7b2
honestoughN <honest.tough18@gmail.com>
2020-11-30 20:42:13 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "hearts#index"
   resources :hearts do
     resources :comments, only: [:new, :create]
+    resource :likes, only: [:create, :destroy]
   end
   resources :users, only: :show
 end
