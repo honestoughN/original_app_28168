8a92ea9221aab55f82901c39b065792139bce1f3
honestoughN <honest.tough18@gmail.com>
2020-11-30 20:48:00 +0900

    コントローラーの設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,18 @@
 class LikesController < ApplicationController
+  before_action :set_heart
+
   def create
+    @like = Like.new(user_id: current_user.id, heart_id: @heart.id)
+    @like.save
   end
 
   def destroy
+    @like = Like.find_by(user_id: :current_user.id, heart_id: @heart.id)
+    @like.destroy
+  end
+
+  private
+  def set_heart
+    @heart = Heart.find_by(id: params[:heart_id])
   end
 end
